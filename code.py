158cb06d5a03b075a79e3d216ea2f452b010ca9f
sshali <106775912+sshali@users.noreply.github.com>
2022-07-07 19:50:04 +0300

    Update code.py

--- code.py
+++ code.py
@@ -6,14 +6,14 @@ import pyperclip
 import wikipedia
 
 
-# coded by kc emma
-# A python programmer
+# coded by Ali
+
 
 
 def search():
     question = my_entry.get()
     language = combobox.get()
-    wikipedia.set_lang('en')  # setting the default language to English Language
+    wikipedia.set_lang('en')  # 
     for k, v in lang_dict.items():
         if language == v:
             wikipedia.set_lang(k)
@@ -47,11 +47,11 @@ lang_dict = googletrans.LANGUAGES
 root = Tk()
 
 root.geometry('700x670+200+10')
-root.title('Wikipedia Search Bar')
+root.title('Wikipedia Arama Barı')
 
 root.config(bg='red4')
 
-my_label = LabelFrame(root, text='Search Wikipedia', bg='red')
+my_label = LabelFrame(root, text='Ara Wikipedia', bg='red')
 my_label.pack(pady=20, padx=22)
 
 my_entry = Entry(my_label, font=('arial', 20, 'bold'))
@@ -61,7 +61,7 @@ combobox = ttk.Combobox(my_label, font=('times new roman ', 20, 'bold'), justify
 combobox.pack()
 
 combobox['values'] = [e for e in lang_dict.values()]
-combobox.set('Select Language')
+combobox.set('Dil Seç')
 
 search_box = Button(my_label, text='SEARCH', font=('Helvetica', 20, 'bold'), command=search)
 search_box.pack(padx=20, pady=10)
@@ -69,16 +69,16 @@ search_box.pack(padx=20, pady=10)
 my_frame = Frame(my_label)
 my_frame.pack(pady=5)
 
-# text_scroll bar
+# 
 text_scrol = Scrollbar(my_frame)
 text_scrol.pack(side=RIGHT, fill=Y)
 
-# text area
+# 
 textarea = Text(my_frame, yscrollcommand=text_scrol.set, wrap='word',
                 font=('Helvetica', 20), height=12, bg='red4', fg='white', state=DISABLED)
 textarea.pack()
 
-# making the scroll bar to be align with the text_bar
+# 
 text_scrol.config(command=textarea.yview)
 
 button_frame = Frame(my_label, bg='red4')
